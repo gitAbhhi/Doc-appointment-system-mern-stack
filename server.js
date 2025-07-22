66bf3cfd478d43002dddc85759dce26d707e758e
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2025-07-22 15:45:56 +0530

    server file and package file update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors =require('colors')
 const moragan=require('morgan')
 const dotenv=require('dotenv');
 const connectDB = require('./config/db');
+const path =require("path")
 
 //dotenv config
 dotenv.config();
@@ -16,12 +17,18 @@ const app=express()
 //middlewares
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(moragan('dev'))
+// app.use(moragan('dev'))
 
 //routes
 app.use("/api/v1/user",require("./routes/userRoutes"));
 app.use('/api/v1/admin',require('./routes/adminRoutes'))
 app.use('/api/v1/doctor',require('./routes/doctorRoutes'))
+
+// deploy purpose
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function(req,res){
+        res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 //port 
 const port =process.env.PORT || 8080
